e675d5d8b6b829c99f34a3a741536db38fab35b6
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-12-12 20:59:07 -0500

    refactor discord utils

--- discord_utils/discord_utils.go
+++ discord_utils/discord_utils.go
@@ -11,35 +11,23 @@ import (
 var session **discordgo.Session
 
 func init() {
-	//fmt.Println('a')
-	//	s := session
-	//	fmt.Printf("%v %p %v\n", &s, s, s)
-
 	session = &config.Discord_Session
-	//	s = session
-	//	fmt.Printf("%v %p %v\n", &config.Discord_Session, config.Discord_Session, *config.Discord_Session)
-
 }
 
 func GetChannelName(ci string) (string, error) {
-	log.Println("aaaa")
 	c, e := (*session).Channel(ci)
-
 	if e != nil {
 		log.Println(e)
 		return "", errors.New("Not Found")
 	}
 
 	return c.Name, nil
-
 }
 
 func CallWebhook(id, token string, data *discordgo.WebhookParams) {
-
 	(*session).WebhookExecute(id, token, false, data)
 }
 func IssueWebhook(ci string) (string, string, error) {
-
 	wh, err := (*session).WebhookCreate(ci, "Groupme Sync", "")
 	if err != nil {
 		return "", "", err
